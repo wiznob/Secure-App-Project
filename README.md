c25f9f951f4a46bbd6bc4edb877e6ff6cf33377d
wiznob <wisbob27@gmail.com>
2025-03-19 15:44:22 +0000

    Secure version made

---
+++ README.md
@@ -0,0 +1,14 @@
+# safe flask website login
+This is the safe login system
+## Prerequisites
+- Python
+- pip
+
+### Gudie
+cd Secure-App-Project
+python -m venv venv (if needed)
+pip install bleach
+pip install bcrypt
+pip install flask
+pip install sqlite3 (if not installed by default)
+python run.py
--- run.py
+++ run.py
@@ -1,1 +1,68 @@
-print("hi")
+from flask import Flask, request, render_template, redirect, url_for
+import sqlite3
+import bcrypt  #password hashing
+import bleach  #input sanitization
+
+app = Flask(__name__)
+
+# Making database
+def init_db():
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS users 
+                 (id INTEGER PRIMARY KEY, username TEXT, password TEXT)''')
+    conn.commit()
+    conn.close()
+
+@app.route("/")
+def welcome():
+    return redirect(url_for('home'))
+
+#login page
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        conn = sqlite3.connect('database.db')
+        c = conn.cursor()
+        # Using parameterized query to avoid SQL injection
+        query = "SELECT password FROM users WHERE username = ?"
+        c.execute(query, (username,))
+        result = c.fetchone()
+        conn.close()
+        # Verifying hashed password
+        if result and bcrypt.checkpw(password.encode('utf-8'), result[0]):
+            return redirect(url_for('home', username=username))
+        else:
+            return "Invalid credentials. Try again."
+
+    return render_template("login.html")
+
+#register page
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+        #sanitizing username input to block a potential XSS attack
+        username = bleach.clean(username)
+        #Using hashing on the password then storing it
+        hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+        #now passwords are hashed we can store them
+        conn = sqlite3.connect('database.db')
+        c = conn.cursor()
+        c.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, hashed_password))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('login'))
+    return render_template("register.html")
+
+@app.route("/home")
+def home():
+    #load homepage
+    return render_template("home.html")
+
+if __name__ == "__main__":
+    init_db()
+    app.run(debug=True)
